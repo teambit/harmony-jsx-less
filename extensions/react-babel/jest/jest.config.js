19faaebc6d73fbde168ff46877badf0c0439d19f
Josh Kuttler <yoshuakuttler@gmail.com>
2020-12-03 13:38:02 +0200

    remove jest setup file

--- extensions/react-babel/jest/jest.config.js
+++ extensions/react-babel/jest/jest.config.js
@@ -2,8 +2,7 @@ const reactJestConfig = require('@teambit/react/jest/jest.config');
 
 module.exports = {
   ...reactJestConfig,
-  // setupFilesAfterEnv: [require.resolve('./jest.setup.js')],
   transformIgnorePatterns: [
-    "/node_modules/(?!(prop-types|@netflux-react-babel))",
+    '/node_modules/(?!(prop-types|@netflux-react-babel))',
   ],
 };
